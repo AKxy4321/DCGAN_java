0aba77f96545e6666634373733322bfea4c03e71
HiddenMachine(Suhas) <HiddenMachine(Suhas)>
2024-05-11 02:30:22 +0530

    changed batchnormalization layer

--- DCGAN/layers/BatchNormalization.java
+++ DCGAN/layers/BatchNormalization.java
@@ -1,14 +1,8 @@
 package DCGAN.layers;
 
-import DCGAN.UTIL;
-
-import java.util.Arrays;
-
-import java.util.Arrays;
-
 public class BatchNormalization {
     private final double epsilon = 1e-5;
-    private final double momentum = 0;
+    private final double momentum = 0.9;
     private double[] runningMean;
     private double[] runningVar;
     private double[] gamma;
@@ -21,8 +15,21 @@ public class BatchNormalization {
 
     double[][] xNormalized;
 
+    // backpropagation variables
+    double[] dMean;
+    double[] dVar;
+
+
     public BatchNormalization(int inputDim) {
-        initializeParameters(inputDim);
+        gamma = new double[inputDim];
+        beta = new double[inputDim];
+        runningMean = new double[inputDim];
+        runningVar = new double[inputDim];
+        this.inputDim = inputDim;
+        for (int i = 0; i < inputDim; i++) {
+            gamma[i] = 1.0;
+            beta[i] = 0.0;
+        }
     }
 
     public double[][] forwardBatch(double[][] x) {
@@ -43,6 +50,7 @@ public class BatchNormalization {
             mean[i] /= batchSize;
         }
 
+
         for (int i = 0; i < inputDim; i++) {
             for (int j = 0; j < batchSize; j++) {
                 xCentered[j][i] = x[j][i] - mean[i];
@@ -53,8 +61,8 @@ public class BatchNormalization {
 
         for (int j = 0; j < batchSize; j++) {
 
-            for(int i=0;i<inputDim;i++)
-                xNormalized[j][i] = xCentered[j][i]/ Math.sqrt(var[i] + epsilon);
+            for (int i = 0; i < inputDim; i++)
+                xNormalized[j][i] = xCentered[j][i] / Math.sqrt(var[i] + epsilon);
 
             for (int i = 0; i < inputDim; i++) {
                 out[j][i] = gamma[i] * xNormalized[j][i] + beta[i];
@@ -82,10 +90,19 @@ public class BatchNormalization {
     }
 
     public double[][] backward(double[][] dout) {
+        /**
+         * Calculates the gradient w.r.t the input to this layer and returns it, but it doesn't update any parameters used here.
+         * Parameters are updated separately in updateParameters function
+         *
+         * reference : https://www.adityaagrawal.net/blog/deep_learning/bprop_batch_norm
+         *
+         * */
+
         double[][] dx = new double[batchSize][inputDim];
         double[][] dxNormalized = new double[batchSize][inputDim];
-        double[] dVar = new double[inputDim];
-        double[] dMean = new double[inputDim];
+        this.dVar = new double[inputDim];
+        this.dMean = new double[inputDim];
+
 
         for (int i = 0; i < dout.length; i++) {
             for (int j = 0; j < dout[0].length; j++) {
@@ -96,56 +113,45 @@ public class BatchNormalization {
             }
         }
 
-        for(int i=0;i<dVar.length;i++) {
-            dVar[i] /= batchSize;
-        }
-
-        for (int i = 0; i < dout.length; i++) {
-            for (int j = 0; j < dout[0].length; j++) {
-                dMean[j] += -1 * (dxNormalized[i][j] / (runningVar[j]+epsilon)
-                        + 2 * (x[i][j] - runningMean[j]) * dVar[j] / batchSize);
+        double[][] xCentered = new double[batchSize][inputDim];
+        for (int i = 0; i < batchSize; i++) {
+            for (int j = 0; j < inputDim; j++) {
+                xCentered[i][j] = x[i][j] - runningMean[j];
             }
         }
 
-        for(int i=0;i<dVar.length;i++) {
-            dMean[i] /= batchSize;
+        double[] xCenteredSum = new double[inputDim];
+        for (int i = 0; i < batchSize; i++) {
+            for (int j = 0; j < inputDim; j++) {
+                xCenteredSum[j] += xCentered[i][j];
+            }
         }
 
         for (int i = 0; i < dout.length; i++) {
             for (int j = 0; j < dout[0].length; j++) {
-                dx[i][j] = (dxNormalized[i][j] / (runningVar[j]+epsilon)
-                        + 2 * (x[i][j] - runningMean[j]) * dVar[j] / batchSize)
+                dMean[j] += -1 * (dxNormalized[i][j] / (Math.sqrt(runningVar[j] + epsilon)))
+                        + dVar[j] * (-2) * xCenteredSum[j] / batchSize;
+            }
+        }
+
+        for (int i = 0; i < dout.length; i++) {
+            for (int j = 0; j < dout[0].length; j++) {
+                dx[i][j] = dxNormalized[i][j] / Math.sqrt(runningVar[j] + epsilon)
+                        + dVar[j] * 2 * (x[i][j] - runningMean[j]) / batchSize
                         + dMean[j] / batchSize;
             }
         }
+
         return dx;
     }
 
     public void updateParameters(double[][] dout, double learning_rate) {
-        double[][] dxNormalized = new double[batchSize][inputDim];
-        double[] dVar = new double[inputDim];
-        double[] dMean = new double[inputDim];
+        /**
+         * Parameters are updated separately in updateParameters function. The input is calculated in the backward function
+         * */
 
         int m = dout.length; // Number of samples in the batch
         int n = dout[0].length; // Number of features
-
-
-        for (int i = 0; i < dout.length; i++) {
-            for (int j = 0; j < dout[0].length; j++) {
-                dxNormalized[i][j] = dout[i][j] * gamma[j];
-
-                dVar[j] += dxNormalized[i][j] * (x[i][j] - runningMean[j])
-                        * (-0.5) * Math.pow(runningVar[j] + epsilon, -1.5);
-            }
-        }
-
-        for (int i = 0; i < dout.length; i++) {
-            for (int j = 0; j < dout[0].length; j++) {
-                dMean[j] += -1 * (dxNormalized[i][j] / (runningVar[j]+epsilon)
-                        + 2 * (x[i][j] - runningMean[j]) * dVar[j] / batchSize);
-            }
-        }
-
 
         double[] dGamma = new double[n];
         double[] dBeta = new double[n];
@@ -166,18 +172,10 @@ public class BatchNormalization {
         }
     }
 
-    private double[] calculateXNormalized(double[] xCentered, double[] var) {
-        double[] xNormalized = new double[xCentered.length];
-        for (int i = 0; i < xCentered.length; i++) {
-            xNormalized[i] = xCentered[i] / Math.sqrt(var[i] + epsilon);
-        }
-        return xNormalized;
-    }
-
     private double[] calculateXNormalized(double[] x, double[] mean, double[] var) {
         double[] xNormalized = new double[x.length];
         for (int i = 0; i < x.length; i++) {
-            xNormalized[i] = (x[i]-mean[i]) / Math.sqrt(var[i] + epsilon);
+            xNormalized[i] = (x[i] - mean[i]) / Math.sqrt(var[i] + epsilon);
         }
         return xNormalized;
     }
@@ -191,17 +189,5 @@ public class BatchNormalization {
     private void updateRunningVar(double[] var) {
         for (int i = 0; i < runningVar.length; i++)
             runningVar[i] = momentum * runningVar[i] + (1 - momentum) * var[i];
-    }
-
-    private void initializeParameters(int inputDim) {
-        gamma = new double[inputDim];
-        beta = new double[inputDim];
-        runningMean = new double[inputDim];
-        runningVar = new double[inputDim];
-        this.inputDim = inputDim;
-        for (int i = 0; i < inputDim; i++) {
-            gamma[i] = 1.0;
-            beta[i] = 0.0;
-        }
     }
 }
