eb18c1eda788b898be9146d9dd1aca2ce7d395bb
HiddenMachine(Suhas) <HiddenMachine(Suhas)>
2024-05-27 21:37:43 +0530

    added spectral normalization to impose lipchitz constraint

---
+++ DCGAN/WGAN_With_SpectralNorm.java
@@ -0,0 +1,148 @@
+package DCGAN;
+
+import DCGAN.networks.Critic;
+import DCGAN.networks.Critic_Spectral_Norm;
+import DCGAN.networks.Generator_Implementation_Without_Batchnorm;
+import DCGAN.util.MiscUtils;
+
+import java.util.logging.Logger;
+
+import static DCGAN.util.MiscUtils.*;
+
+
+public class WGAN_With_SpectralNorm {
+    final private static Logger logger = Logger.getLogger(WGAN_With_SpectralNorm.class.getName());
+
+    int train_size = 1000;
+    int test_size = 5;
+    int label = 3;
+    double learning_rate_gen = 0.00005;
+    double learning_rate_disc = 0.00005;
+
+    private int n_critics = 5;
+
+    int batch_size = 32; // 1 for sgd
+
+    private double disc_loss, gen_loss;
+
+    Generator_Implementation_Without_Batchnorm generator = new Generator_Implementation_Without_Batchnorm(batch_size, learning_rate_gen);
+    Critic_Spectral_Norm critic = new Critic_Spectral_Norm(generator.batchSize * 2, learning_rate_disc);
+
+    public static void main(String[] args) {
+        WGAN_With_SpectralNorm wgan = new WGAN_With_SpectralNorm();
+        wgan.train();
+    }
+
+    public void train() {
+        generator.verbose = true;
+        critic.verbose = true;
+
+        // minibatch gradient descent
+        for (int epochs = 0; epochs < 1000000; epochs++) {
+            int index = 0;// reset our index to 0
+            for (int batch_idx = 0; batch_idx < train_size / batch_size; batch_idx++) {
+                // Load images
+                double[][][][] fakeImages = generator.forwardBatch();
+                double[][][][] realImages = new double[batch_size][1][28][28];
+                for (int real_idx = 0; real_idx < batch_size; real_idx++)
+                    realImages[real_idx] = new double[][][]{
+                            addNoise(
+                                    zeroToOneToMinusOneToOne(img_to_mat(mnist_load_index(label, index++))),
+                                    0.5)
+                    };
+
+                for (int i = 0; i < n_critics; i++) {
+                    train_critic(realImages, fakeImages);
+                }
+
+                // train generator
+                train_generator(critic.forwardBatch(fakeImages));
+
+                // generate image
+                double[][][] gen_img = generator.generateImage();
+                saveImage(getBufferedImage(gen_img), "generated_image_wgan.png");
+
+                // calculating and displaying our model metrics
+                calculateModelMetrics();
+                logger.info("Epoch : " + epochs + " batch:" + batch_idx);
+                logger.info("Gen_Loss : " + gen_loss);
+                logger.info("Disc_Loss : " + disc_loss);
+
+                MiscUtils.saveImage(getBufferedImage(realImages[0]), "real_image_wgan_with_noise.png");
+
+            }
+        }
+    }
+
+    private void train_critic(double[][][][] realImages, double[][][][] fakeImages) {
+        double[][][][] allImages = new double[batch_size * 2][][][];
+        for (int img_idx = 0; img_idx < batch_size; img_idx++) {
+            allImages[img_idx] = realImages[img_idx];
+            allImages[img_idx + batch_size] = fakeImages[img_idx];
+        }
+
+        double[][] outputs = critic.forwardBatch(allImages);
+        // here, the first half are outputs for real images, the second half are outputs for fake images
+        double[][] disc_output_gradients = new double[batch_size * 2][1];
+        for (int img_idx = 0; img_idx < batch_size; img_idx++)
+            disc_output_gradients[img_idx][0] = +1.0 / batch_size; // gradient for output of real images
+        for (int img_idx = batch_size; img_idx < 2 * batch_size; img_idx++)
+            disc_output_gradients[img_idx][0] = -1.0 / batch_size; // gradient for output of fake images
+
+
+        critic.updateParametersBatch(disc_output_gradients);
+
+        // for debugging
+        System.err.printf("Critic output for real : %f, fake : %f\n", outputs[0][0], outputs[batch_size][0]);
+    }
+
+    private double[][] addNoise(double[][] image_array, double scale) {
+
+        for (int i = 0; i < image_array.length; i++) {
+            for (int j = 0; j < image_array[i].length; j++) {
+                image_array[i][j] = clamp(image_array[i][j] + (Math.random() - 0.5) * 2 * scale, -1, 1);
+            }
+        }
+        return image_array;
+    }
+
+
+    public void train_generator(double[][] disc_fake_outputs) {
+        double[][] disc_fake_output_gradients = new double[disc_fake_outputs.length][1];
+        for (int sample_idx = 0; sample_idx < disc_fake_outputs.length; sample_idx++)
+            disc_fake_output_gradients[sample_idx][0] = +1.0 / disc_fake_outputs.length;
+
+        double[][][][] disc_input_gradients_gen_output_gradients = critic.backwardBatch(disc_fake_output_gradients);
+
+        // input to the discriminator is the output of the generator
+        generator.updateParametersBatch(disc_input_gradients_gen_output_gradients);
+
+        // for debugging
+        MiscUtils.saveImage(getBufferedImage(scaleMinMax(disc_input_gradients_gen_output_gradients[0][0])), "critic_in_gradient_wrt_input.png");
+        MiscUtils.prettyprint(disc_input_gradients_gen_output_gradients[0][0]);
+    }
+
+    private void calculateModelMetrics() {
+        double[] fake_outputs = new double[test_size];
+        double[] real_outputs = new double[test_size];
+        int test_index = train_size; // train_size; // we want it to test outside the train dataset
+        for (int img_idx = 0; img_idx < test_size; img_idx++) {
+            real_outputs[img_idx] = critic.getOutput(new double[][][]{zeroToOneToMinusOneToOne(img_to_mat(mnist_load_index(label, test_index++)))})[0];
+            fake_outputs[img_idx] = critic.getOutput(generator.generateImage())[0];
+        }
+
+        disc_loss = criticLoss(real_outputs, fake_outputs);
+        gen_loss = generatorLoss(fake_outputs);
+    }
+
+    public static double criticLoss(double[] real_outputs, double[] fake_outputs) {
+        double avg_real_output = MiscUtils.mean(real_outputs), avg_fake_output = MiscUtils.mean(fake_outputs);
+        return -(avg_real_output - avg_fake_output); // we want to minimize this
+    }
+
+
+    public static double generatorLoss(double[] fake_outputs) {
+        double avg_fake_output = MiscUtils.mean(fake_outputs);
+        return -avg_fake_output / fake_outputs.length;
+    }
+}
---
+++ DCGAN/networks/Critic_Spectral_Norm.java
@@ -0,0 +1,196 @@
+package DCGAN.networks;
+
+import DCGAN.layers.Convolution;
+import DCGAN.layers.DenseLayer;
+import DCGAN.layers.LeakyReLULayer;
+import DCGAN.util.MiscUtils;
+
+import static DCGAN.util.ArrayInitializer.*;
+import static DCGAN.util.MiscUtils.dotProduct;
+import static DCGAN.util.MiscUtils.multiplyScalarInPlace;
+
+public class Critic_Spectral_Norm {
+    Convolution conv1;
+    //    BatchNormalization batch1;
+    LeakyReLULayer leakyReLULayer1;
+    Convolution conv2;
+    //    BatchNormalization batch2;
+    LeakyReLULayer leakyReLULayer2;
+    DenseLayer dense;
+
+    public boolean verbose = false;
+    int batchSize;
+
+    public Critic_Spectral_Norm() {
+        this(1, 1e-3);
+    }
+
+    public Critic_Spectral_Norm(int batchSize, double learning_rate) {
+        this.batchSize = batchSize;
+        int inputWidth = 28, inputHeight = 28;
+        this.conv1 = new Convolution(4, 64, 2,
+                inputWidth, inputHeight, 1,
+                3, 3, 0, learning_rate);
+        this.leakyReLULayer1 = new LeakyReLULayer(0.2);
+        this.conv2 = new Convolution(4, 128, 2,
+                conv1.outputWidth, conv1.outputHeight, conv1.outputDepth,
+                3, 3, 0, learning_rate);
+        this.leakyReLULayer2 = new LeakyReLULayer(0.2);
+        this.dense = new DenseLayer(conv2.outputDepth * conv2.outputWidth * conv2.outputHeight, 1, learning_rate);
+
+
+        outputs_conv1 = new double[batchSize][][][];
+        outputs_leakyRELU1 = new double[batchSize][][][];
+        outputs_conv2 = new double[batchSize][][][];
+        outputs_leakyRELU2 = new double[batchSize][][][];
+        outputs_leakyRELU2_flattened = new double[batchSize][];
+        outputs_dense = new double[batchSize][];
+    }
+
+
+    public double[] getOutput(double[][] black_and_white_image) {
+        double[][][] input = new double[1][][];
+        input[0] = black_and_white_image;
+
+        return getOutput(input);
+    }
+
+
+    public double[] getOutput(double[][][] input) {
+        double[][][] output_conv1 = this.conv1.forward(input);
+        double[][][] output_leakyRELU1 = this.leakyReLULayer1.forward(output_conv1);
+
+        double[][][] output_conv2 = this.conv2.forward(output_leakyRELU1);
+        double[][][] output_leakyRELU2 = this.leakyReLULayer2.forward(output_conv2);
+
+        double[] output_dense = this.dense.forward(MiscUtils.flatten(output_leakyRELU2));
+        return output_dense;
+    }
+
+    double[][][][] inputs;
+    double[][][][] outputs_conv1;
+    double[][][][] outputs_leakyRELU1;
+    double[][][][] outputs_conv2;
+    double[][][][] outputs_leakyRELU2;
+    double[][] outputs_leakyRELU2_flattened;
+    double[][] outputs_dense;
+
+    public double[][] forwardBatch(double[][][][] inputs) {
+        this.inputs = inputs;
+
+        for (int i = 0; i < inputs.length; i++, System.out.print(verbose ? " " + i : "")) {
+            outputs_conv1[i] = this.conv1.forward(inputs[i]);
+            outputs_leakyRELU1[i] = this.leakyReLULayer1.forward(outputs_conv1[i]);
+
+            outputs_conv2[i] = this.conv2.forward(outputs_leakyRELU1[i]);
+            outputs_leakyRELU2[i] = this.leakyReLULayer2.forward(outputs_conv2[i]);
+
+            outputs_leakyRELU2_flattened[i] = MiscUtils.flatten(outputs_leakyRELU2[i]);
+            outputs_dense[i] = this.dense.forward(outputs_leakyRELU2_flattened[i]);
+        }
+
+        return outputs_dense;
+    }
+
+    public double[][][][] backwardBatch(double[][] outputGradients) {
+        double[][][][] disc_in_gradients_conv1 = new double[outputGradients.length][][][];
+
+        for (int i = 0; i < outputGradients.length; i++, System.out.print(verbose ? " " + i : "")) {
+            double[] disc_in_gradient_dense = this.dense.backward(outputGradients[i]);
+
+            double[][][] disc_in_gradient_dense_unflattened = MiscUtils.unflatten(disc_in_gradient_dense, leakyReLULayer2.output.length, leakyReLULayer2.output[0].length, leakyReLULayer2.output[0][0].length);
+            double[][][] disc_in_gradients_leakyrelu2 = this.leakyReLULayer2.backward(disc_in_gradient_dense_unflattened, outputs_leakyRELU2[i]);
+
+            double[][][] disc_in_gradient_conv2 = this.conv2.backward(disc_in_gradients_leakyrelu2);
+            double[][][] disc_in_gradients_leakyrelu1 = this.leakyReLULayer1.backward(disc_in_gradient_conv2, outputs_leakyRELU1[i]);
+
+            disc_in_gradients_conv1[i] = this.conv1.backward(disc_in_gradients_leakyrelu1);
+        }
+
+        return disc_in_gradients_conv1;
+    }
+
+    public void updateParametersBatch(double[][] outputGradients) {
+        double[][][][] disc_in_gradients_leakyrelu2_conv2_out_grad = new double[batchSize][][][];
+        double[][][][] disc_in_gradients_leakyrelu1_conv1_out_grad = new double[batchSize][][][];
+
+        for (int i = 0; i < batchSize; i++) {
+            double[] disc_in_gradient_dense = this.dense.backward(outputGradients[i]);
+
+            double[][][] disc_in_gradient_dense_unflattened = MiscUtils.unflatten(disc_in_gradient_dense, leakyReLULayer2.output.length, leakyReLULayer2.output[0].length, leakyReLULayer2.output[0][0].length);
+            disc_in_gradients_leakyrelu2_conv2_out_grad[i] = this.leakyReLULayer2.backward(disc_in_gradient_dense_unflattened, outputs_leakyRELU2[i]);
+
+            double[][][] disc_in_gradient_conv2 = this.conv2.backward(disc_in_gradients_leakyrelu2_conv2_out_grad[i]);
+            disc_in_gradients_leakyrelu1_conv1_out_grad[i] = this.leakyReLULayer1.backward(disc_in_gradient_conv2, outputs_leakyRELU1[i]);
+
+//            not needed
+//            double[][][] disc_in_gradient_conv1 = this.conv1.backprop(disc_in_gradients_leakyrelu1[i]);
+        }
+
+        dense.updateParametersBatch(outputGradients, outputs_leakyRELU2_flattened);
+        conv2.updateParametersBatch(disc_in_gradients_leakyrelu2_conv2_out_grad, outputs_leakyRELU1);
+        conv1.updateParametersBatch(disc_in_gradients_leakyrelu1_conv1_out_grad, inputs);
+
+        // Applying spectral normalization
+        spectral_norm:
+        {
+            applySpectralNorm(conv1);
+            applySpectralNorm(conv2);
+            applySpectralNorm(dense);
+        }
+
+
+        if (verbose) {
+            // mean of gradients of each layer
+            System.out.println("Mean of output gradients of each layer of critic");
+            System.out.println("dense layer: " + MiscUtils.mean(MiscUtils.flatten(outputGradients)));
+            System.out.println("conv2: " + MiscUtils.mean(MiscUtils.flatten(disc_in_gradients_leakyrelu2_conv2_out_grad[0])));
+            System.out.println("conv1: " + MiscUtils.mean(MiscUtils.flatten(disc_in_gradients_leakyrelu1_conv1_out_grad[0])));
+        }
+    }
+
+    public static void applySpectralNorm(Convolution conv) {
+        multiplyScalarInPlace(conv.filters, getSpectralNorm(conv));
+    }
+
+    public static void applySpectralNorm(DenseLayer layer) {
+        multiplyScalarInPlace(layer.weights, getSpectralNorm(layer));
+    }
+
+    public static double getSpectralNorm(DenseLayer layer) {
+
+        // through power iteration method
+        int n_power_iterations = 5;
+        double[] u_tilde = initRandom1D(layer.outputSize);
+        double[] v_tilde = null;
+        for (int i = 0; i < n_power_iterations; i++) {
+            double[] backwardResult = layer.backward(u_tilde);
+            v_tilde = MiscUtils.multiplyScalarInPlace(backwardResult, 1.0 / MiscUtils.l2Norm(backwardResult));
+
+            double[] forwardResult = layer.forward(v_tilde);
+            u_tilde = MiscUtils.multiplyScalarInPlace(forwardResult, 1.0 / MiscUtils.l2Norm(forwardResult));
+        }
+
+        double spectral_norm = dotProduct(u_tilde, layer.forward(v_tilde));
+
+        return spectral_norm;
+    }
+
+    public static double getSpectralNorm(Convolution conv) {
+        // through power iteration method
+        int n_power_iterations = 5;
+        double[][][] u_tilde = initRandom3D(conv.outputDepth, conv.outputHeight, conv.outputWidth);
+        double[][][] v_tilde = null;
+        for (int i = 0; i < n_power_iterations; i++) {
+            double[][][] backwardResult = conv.backward(u_tilde);
+            v_tilde = MiscUtils.multiplyScalarInPlace(backwardResult, 1.0 / MiscUtils.l2Norm(backwardResult));
+
+            double[][][] forwardResult = conv.forward(v_tilde);
+            u_tilde = MiscUtils.multiplyScalarInPlace(forwardResult, 1.0 / MiscUtils.l2Norm(forwardResult));
+        }
+
+        double spectral_norm = dotProduct(u_tilde, conv.forward(v_tilde));
+
+        return spectral_norm;
+    }
+}
